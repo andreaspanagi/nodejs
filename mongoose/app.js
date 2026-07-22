cbf0e96ee9476f7cc7541a41f3ac875f0b7cd1b5
Andreas <andreaspanagi91@gmail.com>
2026-07-22 11:40:43 +0300

    Add files via upload

---
+++ mongoose/app.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById(process.env.USER_ID)
+    .then(user => {
+        req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then( result => {
+    User.findOne().then(user => {
+      if(!user) {
+          const user = new User({
+            name: 'Andreas',
+            email: 'andreas@mail.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+    });
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
